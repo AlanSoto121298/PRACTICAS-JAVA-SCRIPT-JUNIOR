b51e0224fb63502c3e9ba4c6ad8da1be91c22246
Alan Soto <alansoto121298@gmail.com>
2025-12-30 18:54:35 -0600

    mayor menor que

--- Practica 1 Logica/script.js
+++ Practica 1 Logica/script.js
@@ -19,3 +19,19 @@ function multiplicacion(a, b) {
 const resultado = multiplicacion(5, 5)
 
 console.log(resultado)
+
+//Funcion llamada comparar 2 parametros a,b  si a es mayor  muestra de lo contrario b
+
+function mayorMenor(a, b) {
+    if (a > b) {
+        return "A ES MAYOR QUE B "
+    } else if(a < b ){
+        return "B ES MAYOR QUE A "
+    } else{
+        return "A Y B SON IGUALES"
+    }
+}
+
+const resultado2 = mayorMenor(50,30);
+console.log(resultado2)
+

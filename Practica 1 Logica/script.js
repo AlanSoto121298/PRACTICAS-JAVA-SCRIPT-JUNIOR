30bb322c569db17b94837126d1ecb81e248b3a8b
Alan Soto <alansoto121298@gmail.com>
2025-12-30 18:46:33 -0600

    Ejercicio basicos JS suma y multiplicacion

---
+++ Practica 1 Logica/script.js
@@ -0,0 +1,21 @@
+//Suma en una función con 2 parametros 
+function suma(a, b) {
+
+    const resultado = a + b;
+    console.log(resultado)
+
+}
+
+suma(20, 30)
+
+//Multiplicar con 2 parametros usando return y guardando en variable
+
+function multiplicacion(a, b) {
+
+    return a * b;
+
+}
+
+const resultado = multiplicacion(5, 5)
+
+console.log(resultado)

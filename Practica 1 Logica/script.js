c54b07fc8c737fc893b95c000a3eb3a55fdca323
Alan Soto <alansoto121298@gmail.com>
2025-12-31 08:47:53 -0600

    cambios

--- Practica 1 Logica/script.js
+++ Practica 1 Logica/script.js
@@ -65,7 +65,7 @@ console.log(resultado)
 
 //Escribimos una función con un parametro de un array, vamos agregar los numeros pares a ese array
 
-function numPar(array){
+/* function numPar(array){
     const array2 = [];
 
     for(let i = 0; i < array.length; i++){
@@ -77,5 +77,12 @@ function numPar(array){
     return array2;
 }
 
-const resultado = numPar([1,2,3,4,5,6,7,8,,9,10,11,12,13,14,15,16,17,18,19,20]);
+const resultado = numPar([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]);
 console.log(resultado);
+ */
+//Escribe una función que reciba un array de números y 
+// devuelva un nuevo array con los números mayores que 10.
+
+function numMayor(array){
+
+}

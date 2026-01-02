992fe47ed31a7d97f6939cd089d0356fc3cfc7b2
Alan Soto <alansoto121298@gmail.com>
2026-01-02 09:19:32 -0600

    ejercicio arrays

--- Practica 1 Logica/script.js
+++ Practica 1 Logica/script.js
@@ -98,4 +98,8 @@ console.log(resultado);
 const resultado = numMayor([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
 
 console.log(resultado);
+
  */
+
+/*Escribe una función que reciba un array de números y
+devuelva un nuevo array solo con los números pares. */

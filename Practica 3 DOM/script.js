a39bea97e9b5e2b727fed3f12fc8cf19cf8903bd
Alan Soto <alansoto121298@gmail.com>
2026-01-20 09:56:35 -0600

    Cambiar visible a invisible

--- Practica 3 DOM/script.js
+++ Practica 3 DOM/script.js
@@ -1,5 +1,5 @@
 //Cambiar parrafo con un boton
-const parrafo = document.querySelector("#texto");
+/* const parrafo = document.querySelector("#texto");
 const boton = document.querySelector("#btn");
 
 boton.addEventListener("click",() =>{
@@ -7,10 +7,24 @@ boton.addEventListener("click",() =>{
         parrafo.textContent = "Texto cambiadisimo amigo"
         parrafo.style.color = "red"
     } else{
-        parrafo.textContent = "Texto cambiadisimo amigo"
         parrafo.textContent = "Cambiar"
         parrafo.style.color = "blue"
 
     }
 })
 
+ */
+
+//Cambiar texto de visible a invisible y viseversa 
+
+const parrafo = document.querySelector("#mensaje");
+const boton = document.querySelector("#toggle");
+
+boton.addEventListener("click", () => {
+    if (parrafo.style.display === "none") {
+        parrafo.style.display = "block"
+    } else{
+        parrafo.style.display = "none"
+    }
+
+})

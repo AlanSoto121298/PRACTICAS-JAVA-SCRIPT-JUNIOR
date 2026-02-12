d4e97e228c9c16ac25ea33c60bd6c4113ee72c79
Alan Soto <alansoto121298@gmail.com>
2026-02-12 07:26:02 -0600

    nUMEROS UNICOS FOR

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -1,4 +1,4 @@
-//Funcion que reciba un string y lo devuelve al reves
+//1-Funcion que reciba un string y lo devuelve al reves
 
 /* function reverseString(str){
     let reversed = " ";
@@ -12,10 +12,26 @@
 
 console.log(reverseString("Hola")); */
 
-// Usando reversed con Split reverse y join
+//2- Usando reversed con Split reverse y join
 
-function reverseString(str){
+/* function reverseString(str){
     return str.split("").reverse().join("");
 }
 
-console.log(reverseString("Hola")); 
+console.log(reverseString("Hola"));  */
+
+//3- Escribe una funcion que recibe un array de numeros y devuelve un nuevo array con solo numeros unicos(No duplicados)
+
+function unicos(array){
+    const resultado = [];
+
+    for(let i = 0; i < array.length; i++){
+        if(!resultado.includes(array[i])){
+            resultado.push(array[i])
+        }
+    }
+
+    return resultado
+}
+
+console.log(unicos([1,2,2,3,3,4,5,6,7,9,9,9]))

771d3a889804df1f85a3b371dc9984f10795e3a9
Alan Soto <alansoto121298@gmail.com>
2026-02-13 07:17:18 -0600

    Contar vocales

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -39,7 +39,7 @@ console.log(unicos([1,2,2,3,3,4,5,6,7,9,9,9])) */
 /*4-Escribe una funcion que reciba un array de numeros y devuelve un nuevo array donde cada numero
 aparece tantas veces como su valor  */
 
-function repetir(arr){
+/* function repetir(arr){
     const resultado = [];
 
     for(let i = 0; i < arr.length; i++){
@@ -53,4 +53,22 @@ function repetir(arr){
     return resultado;
 }
 
-console.log(repetir([1,3,2,5]))
+console.log(repetir([1,3,2,5])) */
+
+
+//Funcion que regresa la cantidad de Vocales
+
+function vocales(texto){
+    texto=texto.toLowerCase();
+    let letra = "aeiou";
+    let contador = 0;
+
+    for(let i = 0; i < texto.length; i++){
+        if(letra.includes(texto[i])){
+            contador++
+        }
+    }
+    return contador;
+}
+
+console.log(vocales("Hola buenos dias"))

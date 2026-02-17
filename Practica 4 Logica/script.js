1ddd5677efa6327745477cfa63a2abe2a53ee3c9
Alan Soto <alansoto121298@gmail.com>
2026-02-17 07:20:17 -0600

    Numeros unicos

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -75,12 +75,15 @@ console.log(vocales("Hola buenos dias")) */
 
 
 
-function reverseString(str){
-    let reversed = "";
-    for(let i = str.length -1 ;i >= 0; i--){
-        reversed+=str[i]
+function unicos(array){
+    const resultado = [];
+    for(let i = 0; i < array.length; i++){
+        if(!resultado.includes(array[i])){
+            resultado.push(array[i]);
+        }
     }
-    return reversed;
+
+    return resultado
 }
 
-console.log(reverseString("Mildred"))
+console.log(unicos(([1,2,3,4,5,5,5,6,7,7,8,8,10])))

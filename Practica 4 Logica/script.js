20556068de5ed433fd4420886866558cc8eef363
Alan Soto <alansoto121298@gmail.com>
2026-03-05 08:08:36 -0600

    Num repetido

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -1,5 +1,5 @@
 //Reversed
-function reversedString(str){
+/* function reversedString(str){
     let reversed = "";
     for(let i = str.length -1; i>=0; i--){
         reversed+=str[i]
@@ -8,19 +8,19 @@ function reversedString(str){
     return reversed
 }
 
-console.log(reversedString("Hola"))
+console.log(reversedString("Hola")) */
 
 //Reversed with join ,split and reverse
 
-function reversedString2(str){
+/* function reversedString2(str){
     return str.split("").reverse().join("");
 
 }
 
-   console.log(reversedString2("Alancito"))
+   console.log(reversedString2("Alancito")) */
 
 //Funcion que recibe array y devuelve solo numeros unicos NO DUPLICADOS
-
+/* 
 function unicos(array){
     const resultado = [];
 
@@ -33,4 +33,22 @@ function unicos(array){
     return resultado;
 }
 
-console.log(unicos([1,1,2,3,3,3,5,6,6,6,6]))
+console.log(unicos([1,1,2,3,3,3,5,6,6,6,6])) */
+
+
+//Array que devuelve un nuevo array donde cada numero aparece tantas veces como su valor
+
+function repetir(arr){
+    const resultado = [];
+    for(let i = 0; i < arr.length; i++){
+        const num = arr[i];
+
+        for(let j = 0; j < num; j++){
+            resultado.push(num)
+        }
+
+        return resultado
+    }
+}
+
+console.log(repetir([1,3,2]))

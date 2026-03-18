97d14bf38e9ab5b6c54b80108614a735c5afab26
Alan Soto <alansoto121298@gmail.com>
2026-03-18 08:14:27 -0600

    Num repetidos

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -1,6 +1,6 @@
 //Reversed
 
-function reverseString(str){
+/* function reverseString(str){
     let reversed = "";
     for(let i = str.length -1; i >= 0; i--){
         reversed += str[i];
@@ -10,12 +10,29 @@ function reverseString(str){
 }
 
 console.log(reverseString("Alancio"))
+ */
 
 //Reversed con split
 
-function reverseStringTwo(str){
+/* function reverseStringTwo(str){
     return str.split("").reverse().join("");
     
 }
 
-console.log(reverseStringTwo("Help"))
+console.log(reverseStringTwo("Help")) */
+
+//Devuelva un array sin numeros duplicados
+
+function unicos(array){
+    const resultado = [];
+    for(let i = 0; i < array.length; i++){
+        if(!resultado.includes(array[i])){
+            resultado.push(array[i])
+        }
+    }
+
+    return resultado;
+
+}
+
+console.log(unicos([1,1,2,3,3,3,4,5,8,9,10,10,11]))

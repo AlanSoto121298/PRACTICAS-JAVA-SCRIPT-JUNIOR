6f702138c40609ff4ece36e65b98e0ea3c3aa851
Alan Soto <alansoto121298@gmail.com>
2026-03-18 08:18:19 -0600

    ParImpar

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -23,7 +23,7 @@ console.log(reverseStringTwo("Help")) */
 
 //Devuelva un array sin numeros duplicados
 
-function unicos(array){
+/* function unicos(array){
     const resultado = [];
     for(let i = 0; i < array.length; i++){
         if(!resultado.includes(array[i])){
@@ -35,4 +35,22 @@ function unicos(array){
 
 }
 
-console.log(unicos([1,1,2,3,3,3,4,5,8,9,10,10,11]))
+console.log(unicos([1,1,2,3,3,3,4,5,8,9,10,10,11])) */
+
+//Num par
+
+function parImpar(array){
+    const pares = [];
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            pares.push(array[i])
+        }
+    }
+
+    return pares;
+}
+
+const resultado = parImpar([1,2,3,4,5,6,7,8,9,10])
+
+console.log(resultado)

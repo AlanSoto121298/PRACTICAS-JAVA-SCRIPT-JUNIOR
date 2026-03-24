fbda7c68f171def9870fe40a2b808afcba06176d
Alan Soto <alansoto121298@gmail.com>
2026-03-23 19:43:35 -0600

    Palabras al reves

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -1,75 +1,12 @@
-//Reversed
-
-/* function reverseString(str){
+//Funcion que reciba string y de vuelva al re vez
+function reverseString(str){
     let reversed = "";
-    for(let i = str.length -1; i >= 0; i--){
-        reversed += str[i];
+
+    for(let i = str.length -1; i >=0  ; i--){
+        reversed+=str[i];
     }
 
-    return reversed ;
+    return reversed
 }
 
-console.log(reverseString("Alancio"))
- */
-
-//Reversed con split
-
-/* function reverseStringTwo(str){
-    return str.split("").reverse().join("");
-    
-}
-
-console.log(reverseStringTwo("Help")) */
-
-//Devuelva un array sin numeros duplicados
-
-/* function unicos(array){
-    const resultado = [];
-    for(let i = 0; i < array.length; i++){
-        if(!resultado.includes(array[i])){
-            resultado.push(array[i])
-        }
-    }
-
-    return resultado;
-
-}
-
-console.log(unicos([1,1,2,3,3,3,4,5,8,9,10,10,11])) */
-
-//Num par
-
-/* function parImpar(array){
-    const pares = [];
-
-    for(let i = 0; i < array.length; i++){
-        if(array[i] % 2 === 0){
-            pares.push(array[i])
-        }
-    }
-
-    return pares;
-}
-
-const resultado = parImpar([1,2,3,4,5,6,7,8,9,10])
-
-console.log(resultado) */
-
-//Reciba un array de numeros y devuelva un nuevo array donde cada num aparece tantas v eces como su valor
-
-function repetir(arr){
-    const resultado = [];
-    for(let i = 0; i <arr.length; i++){
-        const num = arr[i];
-
-        for(let j = 0; j < num; j++){
-            resultado.push(num);
-        }
-
-        
-    }
-
-    return resultado;
-}
-
-console.log(repetir([1,3,2]))
+console.log(reverseString("Hola"))

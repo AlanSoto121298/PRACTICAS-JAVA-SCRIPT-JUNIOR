720d4a39ebbd4650009c6131d184d53a08e789d7
Alan Soto <alansoto121298@gmail.com>
2026-03-30 18:34:51 -0600

    Nums duplicados

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -12,7 +12,23 @@
 console.log(reverseString("Alan")) */
 
 
-function reverseString(str){
+/* function reverseString(str){
     return str.split("").reverse().join("")
 }
-console.log(reverseString("Alan")) 
+console.log(reverseString("Alan"))  */
+
+
+//Function unicos sin duplicados
+
+function unicos(array){
+    const resultado = [];
+    for(let i = 0; i < array.length; i++){
+        if(!resultado.includes(array[i])){
+            resultado.push(array[i])
+        }
+    }
+
+    return resultado
+}
+
+console.log(unicos([1,1,2,3,4,4,4,5,5,5,5,7,10]))

294574feb852c7eba382784a6b0e537fd9695304
Alan Soto <alansoto121298@gmail.com>
2026-04-07 07:15:17 -0600

    Quitar numeros duplicados

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -12,9 +12,26 @@
 console.log(reverseString("Hola")) */
 
 //Reversed con Split
-function reverseString(str){
+/* function reverseString(str){
     return str.split("").reverse().join("");
     
 }
 
-console.log(reverseString("Help"))
+console.log(reverseString("Help")) */
+
+
+//Numeros duplicados
+
+function unicos(array){
+    const resultado = [];
+
+    for(let i = 0; i < array.length; i++){
+        if(!resultado.includes(array[i])){
+            resultado.push(array[i])
+        }
+    }
+
+    return resultado;
+}
+
+console.log(unicos([1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,8,8,9,10]))

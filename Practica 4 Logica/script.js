9ef04196062c8c7b174a32cf892051e6835c0456
Alan Soto <alansoto121298@gmail.com>
2026-04-07 07:10:21 -0600

    Reversed

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -1,34 +1,12 @@
-//funcion que resiva un string al reves
+//Funcion que rec iba un string y devuelva al reves
 
-/* function reverseString(str){
+function reverseString(str){
     let reversed = "";
-    for(let i = str.length -1; i>=0; i--){
-       reversed+=str[i]
+    for(let i = str.length -1; i>= 0; i--){
+        reversed+=str[i]
     }
 
     return reversed;
 }
 
-console.log(reverseString("Alan")) */
-
-
-/* function reverseString(str){
-    return str.split("").reverse().join("")
-}
-console.log(reverseString("Alan"))  */
-
-
-//Function unicos sin duplicados
-
-function unicos(array){
-    const resultado = [];
-    for(let i = 0; i < array.length; i++){
-        if(!resultado.includes(array[i])){
-            resultado.push(array[i])
-        }
-    }
-
-    return resultado
-}
-
-console.log(unicos([1,1,2,3,4,4,4,5,5,5,5,7,10]))
+console.log(reverseString("Hola"))

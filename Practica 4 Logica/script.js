69a39c2940d2478217a40eaa368419ce672cca61
Alan Soto <alansoto121298@gmail.com>
2026-04-14 19:29:19 -0600

    REVERSED

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -1,87 +1,12 @@
-//Funcion que rec iba un string y devuelva al reves
+//Funcion que reciba string al reves
 
-/* function reverseString(str){
+function reverseString(str){
     let reversed = "";
-    for(let i = str.length -1; i>= 0; i--){
+    for(let i = str.length -1; i >=0; i--){
         reversed+=str[i]
     }
 
-    return reversed;
+    return reversed
 }
 
-console.log(reverseString("Hola")) */
-
-//Reversed con Split
-/* function reverseString(str){
-    return str.split("").reverse().join("");
-    
-}
-
-console.log(reverseString("Help")) */
-
-
-//Numeros duplicados
-
-/* function unicos(array){
-    const resultado = [];
-
-    for(let i = 0; i < array.length; i++){
-        if(!resultado.includes(array[i])){
-            resultado.push(array[i])
-        }
-    }
-
-    return resultado;
-}
-
-console.log(unicos([1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,8,8,9,10])) */
-
-//Numero aparece como su valor
-
-/* function repetir(arr){
-    const resultado = [];
-    for(let i = 0; i < arr.length; i++){
-        const num = arr[i];
-
-        for(let j = 0; j < num; j++){
-            resultado.push(num)
-        }
-    }
-     return resultado;
-}
-
-console.log(repetir([1,2,3,5,6,7,10])) */
-
-//Array de numeros y suma total
-/* 
-function sumArray(numeros){
-    let suma = 0;
-    for(let i = 0; i < numeros.length; i++){
-        suma +=numeros[i]
-    }
-
-    return suma;
-}
-
-console.log(sumArray([1,2,4,5,6,100])) */
-
-//Array con pares
-
-/* function filtrarPares(array){
-    const pares = [];
-
-    for(let i = 0; i < array.length; i++){
-        if(array[i] % 2 == 0){
-            pares.push(array[i])
-        }
-    }
-
-    return pares;
-} */
-
-/* const resultado = filtrarPares([1,2,3,4,5,6,7,8,9,10])
-console.log(resultado) */
-
-const filtrarPares = arr => arr.filter(n => n % 2 == 0)
-const resultado = filtrarPares([1,2,3,4,5,6,7,8,9,10])
-console.log(resultado)
+console.log(reverseString("Hola alan"))

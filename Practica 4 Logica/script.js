794ab06fcd86c3f8dd82f1deb62d5c597899791d
Alan Soto <alansoto121298@gmail.com>
2026-06-02 10:41:18 -0600

    Reverse normal

--- Practica 4 Logica/script.js
+++ Practica 4 Logica/script.js
@@ -1,18 +1,14 @@
 //Funcion que reciba string al reves
 
-/* function reverseString(str){
+
+function reverseString(str){
     let reversed = "";
-    for(let i = str.length -1; i >=0; i--){
+    for(let i = str.length -1; i >= 0; i--){
         reversed+=str[i]
     }
 
-    return reversed
+    return reversed;
 }
 
-console.log(reverseString("Hola alan")) */
+console.log(reverseString("Hola alan"))
 
-function  reverseString(str){
-    return str.split("").reverse().join("");
-}
-
-console.log(reverseString("Hola"))
